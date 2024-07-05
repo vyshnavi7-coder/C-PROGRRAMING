76cbe91c42e40680e1cb8980a1b9e4e361e3cbf4
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2024-07-05 10:25:55 +0530

    Add files via upload

---
+++ vyshu.001.c
@@ -0,0 +1,30 @@
+//program to print your name,date of birth,and mobile number
+
+#include<stdio.h>
+main()
+{
+    int DOB,mobileno;
+    char name;
+
+
+    printf("enter your name:");
+    scanf("%s",&name);
+
+    printf("enter your DOB:");
+    scanf("%d",&DOB);
+
+    printf("enter mobile no:");
+    scanf("%d",&mobileno);
+
+    //output:
+    printf("name is: %s",name);
+    printf("DOB is: %d",DOB);
+    printf("mobile no is: %d",mobileno);
+
+    printf("\n")
+
+    p
+}
+
+
+
